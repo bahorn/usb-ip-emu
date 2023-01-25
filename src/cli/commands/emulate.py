42bf810a780b9ec694e51522fee921dd71a017b4
B Horn <b@horn.uk>
2023-01-25 04:07:09 +0000

    Supporting directories of pcaps
    Signed-off-by: B Horn <b@horn.uk>

--- src/cli/commands/emulate.py
+++ src/cli/commands/emulate.py
@@ -2,6 +2,7 @@
 Entrypoint.
 """
 import click
+import os
 from usbip import USBIPServer
 from devices.emulateddevice import EmulatedDevice
 from device.devicelist import DeviceList
@@ -13,7 +14,20 @@ from device.devicelist import DeviceList
 @click.argument('pcaps', nargs=-1)
 def emulate(host, port, pcaps):
     device_list = DeviceList()
-    emulated_device = EmulatedDevice(pcaps)
+
+    # support either pcapng's or directories with pcapngs.
+    pcaps_ = []
+    for pcap in pcaps:
+        if not os.path.isdir(pcap):
+            pcaps_.append(pcap)
+
+        for result in os.listdir(pcap):
+            pcaps_.append(os.path.join(pcap, result))
+
+    if len(pcaps_) == 0:
+        return
+
+    emulated_device = EmulatedDevice(pcaps_)
     device_list.register((1, 1), emulated_device)
     usb_server = USBIPServer(host, port, device_list)
     usb_server.start()
